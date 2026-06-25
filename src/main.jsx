a0025d6ed47da8fe4f88f72904d27a8cfdf7c27a
AKASH7455 <ubayveer5@gmail.com>
2026-06-25 13:51:21 +0530

    analytics add kiya

--- src/main.jsx
+++ src/main.jsx
@@ -1,43 +1,30 @@
 import { StrictMode } from "react";
-
 import { createRoot } from "react-dom/client";
-
 import { BrowserRouter } from "react-router-dom";
 
 import { LanguageProvider } from "./context/LanguageContext";
 import { ProgressProvider } from "./context/ProgressContext";
 import { ThemeProvider } from "./context/ThemeContext";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import App from "./App";
 
 import "./styles/variables.css";
-
 import "./styles/global.css";
-
 import "./styles/themes.css";
 
-
-
 createRoot(document.getElementById("root")).render(
-
   <StrictMode>
-
     <ThemeProvider>
       <LanguageProvider>
         <ProgressProvider>
-
-      <BrowserRouter>
-
-        <App />
-
-      </BrowserRouter>
-
-    </ProgressProvider>
+          <BrowserRouter>
+            <App />
+            <Analytics />
+          </BrowserRouter>
+        </ProgressProvider>
       </LanguageProvider>
     </ThemeProvider>
-
   </StrictMode>
-
 );
-
-
